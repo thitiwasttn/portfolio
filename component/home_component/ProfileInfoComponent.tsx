d4e33e71df7c05197de92f9a293b26e96270c4c4
Thitiwas Nupan <twopee26@gmail.com>
2024-05-25 23:00:54 +0700

    update

--- component/home_component/ProfileInfoComponent.tsx
+++ component/home_component/ProfileInfoComponent.tsx
@@ -7,56 +7,81 @@ import emailIcon from '@/public/image/email.png'
 import telephoneIcon from '@/public/image/telephone.png'
 import SocialMediaButtons from "@/component/home_component/SocialMediaButtons";
 
+interface ProfileInfoM {
+    name: string,
+    rolePosition: string,
+    email: string,
+    telno: string
+}
 
 export default function ProfileInfoComponent() {
 
-    const name: string = 'Thitiwas Nupan'
-    const rolePosition: string = 'Software Developer '
+    const profileM: ProfileInfoM = {
+        email: "twopee26@gmail.com", name: "Thitiwas Nupan", rolePosition: "Software Developer", telno: "094-251-8661"
+    }
     return (
         <>
             <div className={"profileInfo shadow bg-body-tertiary rounded"}>
-                <div className={"container "}>
+                <div className={"container h-100 d-inline-block position-relative"}>
                     <div className={"row mt-5"}>
-                        <div className={"col-12"}>
-                            <img src={avatar.src} className="rounded mx-auto d-block" alt="..."/>
+                        <div className={"col-12 text-center"}>
+                            <img src={avatar.src} className="rounded mx-auto d-block img-thumbnail img-fluid" alt="..."/>
                         </div>
                         <div className={"col-12 mt-3"}>
-                            <span className="fs-3 d-flex justify-content-center">{name}</span>
+                            <span className="fs-3 d-flex justify-content-center">{profileM.name}</span>
                         </div>
                         <div className={"col-12 mt-2"}>
                             <span
-                                className="fs-5 d-flex justify-content-center fst-italic text-body-secondary">{rolePosition}</span>
+                                className="fs-5 d-flex justify-content-center fst-italic text-body-secondary">{profileM.rolePosition}</span>
                         </div>
                         <div className={"col-12"}>
                             <hr/>
                         </div>
                         <div className={"col-12 mt-2"}>
                             <div className={"d-flex align-items-center"}>
-                                <span className={"ms-3 me-2"}>
+                                <div>
+                                    <span className={"ms-3 me-2"}>
                                     <img src={home.src} height={16} width={16} alt=""/>
                                 </span>
-                                <span className={"me-2"}>Bangkok, Thailand</span>
-                                <img src={thailandFlag.src} height={16} width={16} alt=""/>
+                                    <span className={"me-2"}>Bangkok, Thailand</span>
+                                    <img src={thailandFlag.src} height={16} width={16} alt=""/>
+                                </div>
                             </div>
                         </div>
 
                         <div className={"col-12 mt-2"}>
                             <div className={"d-flex align-items-center "}>
-                                <img className={"ms-3 me-2"} src={emailIcon.src} height={17} width={17} alt=""/>
-                                <span className={"me-2"}>thitiwas.nn@gmail.com</span>
+                                <div>
+                                    <img className={"ms-3 me-2"} src={emailIcon.src} height={17} width={17} alt=""/>
+                                    <span className={"me-2"}>{profileM.email}</span>
+                                </div>
                             </div>
                         </div>
 
                         <div className={"col-12 mt-2"}>
                             <div className={"d-flex align-items-center "}>
-                                <img className={"ms-3 me-2"} src={telephoneIcon.src} height={17} width={17} alt=""/>
-                                <span className={"me-2"}>094-251-8661</span>
+                                <div>
+                                    <img className={"ms-3 me-2"} src={telephoneIcon.src} height={17} width={17}
+                                         alt=""/>
+                                    <span className={"me-2"}>{profileM.telno}</span>
+                                </div>
                             </div>
                         </div>
+
+                        <div className={"col-12 mt-2 d-block d-md-none mb-3 "}>
+                            <div className={"align-items-center "}>
+                                <SocialMediaButtons/>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*<div className={"bottom-social"}>
+
+                    </div>*/}
+                    <div className="position-absolute bottom-0 start-50 translate-middle-x mb-3 d-none d-md-block">
                         <SocialMediaButtons/>
                     </div>
                 </div>
-
             </div>
         </>
     )
