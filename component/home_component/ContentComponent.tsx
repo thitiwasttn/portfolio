f71e09df04adddffbfc5ba9cce6042cb44b582c9
Thitiwas Nupan <twopee26@gmail.com>
2024-06-16 00:35:39 +0700

    update

--- component/home_component/ContentComponent.tsx
+++ component/home_component/ContentComponent.tsx
@@ -1,7 +1,40 @@
-export default function ContentComponent() {
-    return <>
-        <div className={"profileInfo shadow bg-body-tertiary rounded-4"}>
+import SkillComponent from "@/component/home_component/skill/SkillComponent";
 
+export default function ContentComponent() {
+    const listAreaOfExp: string[] = [
+        "Backend developer",
+        "Web developer",
+        "Java developer",
+        "Windows server administration",
+        "Linux server administration"
+    ]
+    return <>
+        <div className={"profileInfo shadow bg-body-tertiary rounded-4 overflow-auto"}>
+            <div className={"container-fluid "}>
+                <div className={"row mt-5"}>
+                    <div className={"col-12"}>
+                        <p className="h4">AREAS OF EXPERTISE</p>
+                    </div>
+                    <div className={"col-12"}>
+                        <ul className="list-group list-group-flush rounded-4">
+                            {listAreaOfExp.map(value => {
+                                return <li key={value} className="list-group-item">{value}</li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
+
+
+                {/*    skill */}
+                <div className={"row mt-3"}>
+                    <div className={"col-12"}>
+                        <p className="h4">SKILLS</p>
+                    </div>
+                    <div className={"col-12"}>
+                        <SkillComponent/>
+                    </div>
+                </div>
+            </div>
         </div>
     </>
 }
--- component/home_component/DownloadCvComponent.tsx
+++ component/home_component/DownloadCvComponent.tsx
@@ -1,7 +1,13 @@
+'use client'
 import './DownloadCv.css'
 
 export default function DownloadCvComponent() {
+    const linkCV: string = "https://drive.google.com/file/d/1ID_sNAynq6y8GHXasc1mPi8l2F_jSEND/view?usp=drive_link";
+    function downloadCV() {
+        const newWindow = window.open(linkCV, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
     return <>
-        <button id="bottone5">Download CV</button>
+        <button id="bottone5" onClick={downloadCV}>Download CV</button>
     </>
 }
