653769f6138aa07747711b89e6a5d7d46751c98d
Work <twopee26@gmail.com>
2024-06-29 17:00:14 +0700

    update

--- component/home_component/skill/SkillComponent.tsx
+++ component/home_component/skill/SkillComponent.tsx
@@ -8,24 +8,68 @@ interface SkillM {
 }
 
 interface Detail {
-    value: string
+    title: string
+    detail: string
 }
 
 export default function SkillComponent() {
-    const skills: SkillM[] = [{
-        display: "Programming skills", value: "programmingSkill",
-        details: [
-            {value: "Expert Java (spring, spring boot)"},
-            {value: "Expert SQL"},
-            {value: "Expert JavaScript, TypeScript (React, Angular, Jquery)"}
-        ]
-    }, {
-        display: "Database", value: "database",
-    }, {
-        display: "OS", value: "os",
-    }, {
-        display: "Tools", value: "tool",
-    }]
+    const skills: SkillM[] = [
+        {
+            display: "Programming skills",
+            value: "programmingSkill",
+            details: [
+                {
+                    title: "Expert Java",
+                    detail: "Spring, Spring boot"
+                },
+                {
+                    title: "Expert SQL",
+                    detail: "-"
+                },
+                {
+                    title: "Expert JavaScript, TypeScript",
+                    detail: "React, Next.js, Angular, Jquery"
+                },
+                {
+                    title: "Python",
+                    detail: "-"
+                },
+                {
+                    title: "HTML, CSS",
+                    detail: "Bootstrap CSS framework"
+                },
+                {
+                    title: "Basic Predictive Machine Learning",
+                    detail: "-"
+                },
+                {
+                    title: "PHP",
+                    detail: "-"
+                }
+            ]
+        }, {
+            display: "Database",
+            value: "database",
+            details: [
+                {
+                    title: "MySQL",
+                    detail: "-"
+                },
+                {
+                    title: "PostgresSQL",
+                    detail: "-"
+                },
+                {
+                    title: "Oracle",
+                    detail: "-"
+                },
+            ]
+        }, {
+            display: "OS", value: "os",
+        }, {
+            display: "Tools", value: "tool",
+        }
+    ]
 
     return (
         <>
@@ -36,24 +80,17 @@ export default function SkillComponent() {
                         <Accordion.Item key={value.value} eventKey={value.value}>
                             <Accordion.Header>{value.display}</Accordion.Header>
                             <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                culpa qui officia deserunt mollit anim id est laborum.
-
                                 <div className={"row"}>
-                                    <ul className="list-group list-group-horizontal flex-fill list-group-numbered">
-                                        <li className="col list-group-item">An item</li>
-                                        <li className="col list-group-item">A second item</li>
-                                        <li className="col list-group-item">A third item</li>
-                                        <li className="col list-group-item">A second item</li>
-                                        <li className="col list-group-item">A third item</li>
-                                        <li className="col list-group-item">A second item</li>
-                                        <li className="col list-group-item">A third item</li>
-                                    </ul>
+                                    {value.details?.map(value1 => {
+                                        return <div key={value1.title} className={"col-sm-auto col-md-4 col-xl-4 mt-2"}>
+                                            <div className="card h-100">
+                                                <div className="card-body">
+                                                    <h6 className="card-title">{value1.title}</h6>
+                                                    <p className="card-text">{value1.detail}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    })}
                                 </div>
                             </Accordion.Body>
                         </Accordion.Item>
